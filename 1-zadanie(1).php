82ee0a38b208616f2e1f424d96cb7ae50891f377
Aldiyar200413 <99604967+Aldiyar200413@users.noreply.github.com>
2022-04-02 13:28:07 +0600

    Add files via upload

---
+++ 1-zadanie(1).php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>1-zadanie</title>
+</head>
+<body>
+	<?php
+	$x=70;
+    $y=35;
+    $z=$x/$y;
+    $q=10;
+    $num=$z*$q;
+    echo "Z=",$z;
+    echo "NUM=",$num;
+	?>
+</body>
+</html>
